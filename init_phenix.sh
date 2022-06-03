8898f082b0ddc15d89979ab82e78cefa4eed58f0
Russ Taylor <russ@reliasolve.com>
2022-06-03 11:55:08 -0400

    Adding init for Phenix build

---
+++ init_phenix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ~/rlab/phenix/build/setpaths.sh
+
+#source ~/rlab/cctbx-native-molprobity-python27/build/setpaths.sh
+#export MMTBX_CCP4_MONOMER_LIB=/home/taylorr/rlab/phenix/modules/chem_data/geostd
+#export CLIBD_MON=/home/taylorr/rlab/phenix/modules/chem_data/mon_lib
